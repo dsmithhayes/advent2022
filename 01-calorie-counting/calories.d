68190ef6e16474d624380773dcd32c569edfe0ca
Dave Smith-Hayes <me@davesmithhayes.com>
2022-12-03 17:38:23 -0500

    Set up functions to split out lists of integers and parse strings as integers.

--- 01-calorie-counting/calories.d
+++ 01-calorie-counting/calories.d
@@ -1,8 +1,10 @@
-import std.stdio;
+import std.stdio, std.regex, std.array, 
+    std.algorithm, std.conv, std.string;
 
-int tallCalories(int[] calories)
+int tallyCalories(int[] calories)
 {
     int total = 0;
+
     for (size_t i = 0; i < calories.length; i++) {
         total += calories[i];
     }
@@ -10,9 +12,41 @@ int tallCalories(int[] calories)
     return total;
 }
 
+string[] parseLists(string list)
+{
+    return std.regex.split(list, regex("\n\n"));
+}
+
+int[][] convertList(string[] caloriesList) 
+{
+    int[][] buffer = [];
+    for (size_t i = 0; i < caloriesList.length; i++) {
+        string[] values = splitLines(caloriesList[i]);
+        buffer[i]= values.map!(to!int).array;
+    }
+    return buffer;
+}
+
 unittest {
     int[] calories = [ 1000, 2000, 3000 ];
-    assert(6000 == tallCalories(calories));
+    assert(6000 == tallyCalories(calories));
+
+    string caloriesList = "
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+";
+
+    string[] parsedCaloriesList = parseLists(caloriesList);
+    assert(parsedCaloriesList[1] == "4000");
+
+    int[][] listsAsNumbers = convertList(parsedCaloriesList);
+    assert(listsAsNumbers[0][0] == 1000);
 }
 
 void main()
